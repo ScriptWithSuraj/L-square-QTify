8b6c4472b7e26be7726281820470b25c2a25acc0
Suraj <coolsuraj96@gmail.com>
2023-03-09 21:43:05 +0530

    C2:Button

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Logo() {
+  return <img src={require("../../assets/logo.png")} alt="logo" width={67} />;
+}
+export default Logo;
---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../Button/Button";
+import Logo from "../Logo/Logo";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <Logo />
+      <Button>Give Feedback</Button>
+    </div>
+  );
+}
+
+export default Navbar;
