a3dbd56d4b101af20f7fe590cda46e6d7bcfc461
Suraj <coolsuraj96@gmail.com>
2023-03-15 23:45:56 +0530

    Grid Cards

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -22,7 +22,7 @@ function Section({ title, dataSource }) {
           <h4>{title}</h4>
         </div>
         <div className={styles.showAll} onClick={carouselToggle}>
-          {!showAll ? <h4>Show All</h4> : <h4>Collapse</h4>}
+          {<h4>{!showAll ? "Collapse" : "ShowAll"}</h4>}
         </div>
       </div>
       <div className={styles.sectionCard}>
